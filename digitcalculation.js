450a2f7e1f3a123964d9e17b4ef7bcacf75dbffa
rijorobins <30646223+rijorobins@users.noreply.github.com>
2020-11-03 11:56:36 +0530

    Add files via upload

---
+++ digitcalculation.js
@@ -0,0 +1,26 @@
+//var num=2
+//op=(num*10+num)+num
+//console.log(op)
+
+/*var num=3
+op=num+(num*10+num)+(num*100+num*10+num)
+console.log(op)
+
+var num=4
+op1=num+(num*10+num)+(num*100+num*10+num)+(num*1000+num*100+num*10+num)
+console.log(op1)*/
+
+var num=Number(prompt("enter number:"))
+if(num==4){
+    op1=num+(num*10+num)+(num*100+num*10+num)+(num*1000+num*100+num*10+num)
+    alert(op1)
+}
+else if(num==3){
+    op=num+(num*10+num)+(num*100+num*10+num)
+    alert(op)
+}
+else if(num=2){
+    opt=(num*10+num)+num
+    alert(opt)
+
+}
---
+++ powerCubepgm.js
@@ -0,0 +1,14 @@
+var exponent_num=3
+var minimum=10
+var maximum=400
+
+for(i=1;i<10;i++)
+{
+    pwr=i**exponent_num
+    if(pwr >= minimum & pwr <= maximum)
+    {
+        console.log(pwr)
+    }
+
+}
+
